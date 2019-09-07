723db9c7d2c7d6c1a5fb50a960cc1e6a01a6d4b4
Yuru Kristen Song <55000401+yuru-eats-celery@users.noreply.github.com>
2019-09-07 14:34:20 -0700

    Update plot_fig_5.m

--- plot_fig_5.m
+++ plot_fig_5.m
@@ -1,6 +1,6 @@
 clear all;
-data = {'a','b','c','d','e'};
-for fig = 1: 5
+data = {'a','b','c','d','e','f'};
+for fig = 1: 6
     file_name = strcat('figure_5',data{fig},'.mat');
     save_fig_name = strcat('figure_5',data{fig});
     load(file_name);
@@ -9,7 +9,7 @@ for fig = 1: 5
             C_uv(j,i) = corr(reshape(sum_u(1,j, :, i),[numel(sum_u(1,j, :, i)),1]), reshape(sum_v(1,j, :, i),[numel(sum_v(1,j, :, i)),1]));
         end
     end
-    figure(fig);
+    figure(fig); 
     errorbar(linspace(0, 10, 50),mean(C_uv,1), std(C_uv)/sqrt(10));
 %     title(data{fig}); 
     if mod(fig,2) == 1
@@ -17,9 +17,21 @@ for fig = 1: 5
     else
         xlabel('\Pi_1, \Pi_3');
     end
-    ylabel('corr(A_u,A_v)');
+     ylabel('corr(A_u,A_v)')
+    if fig == 1
+        title('Spontaneous Pain Correlation Bet. A_{u} and A_{v} w. constant \pi_{1} and \pi_{3}')
+    elseif fig == 2
+        title('Spontaneous Pain Correlation Bet. A_{u} and A_{v}  w. constant \pi_{2}');
+    elseif fig == 3
+        title('Placebo Pain Correlation Bet. A_{u} and A_{v} w. constant \pi_{1} and \pi_{3}');
+    elseif fig == 4
+        title('Placebo Pain Correlation Bet. A_{u} and A_{v} w. constant \pi_{2}');
+    elseif fig ==5
+        title('Evoked Pain Correlation Bet. A_{u} and A_{v} w. constant \pi_{1} and \pi_{3}');
+    elseif fig ==6
+        title('Evoked Pain Correlation Bet. A_{u} and A_{v} w. constant \pi_{2}');
+    end
     set(gcf, 'Units','Normalized','OuterPosition',[(fig-1)*.22 + .1 ,.6,.21,.35]);
     saveas(fig, save_fig_name,'epsc');
     saveas(fig, save_fig_name,'fig');
 end
-% pause;close all;
